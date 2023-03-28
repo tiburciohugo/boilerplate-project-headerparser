681612f0a55638621084fec856dafb25da5e21f4
Hugo Tiburcio <hugotiburcio@outlook.com>
2023-03-28 13:25:27 -0300

    Initial Commit

--- index.js
+++ index.js
@@ -24,6 +24,14 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// Header parser
+app.get('/api/whoami', (req, res) => {
+  const ip = req.ip;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.json({ ipaddress: ip, language, software });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
